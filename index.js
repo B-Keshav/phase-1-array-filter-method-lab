1b37156786e559d4829e4750405c58ea57dcb2c9
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-30 13:53:01 -0400

    commit tree c323f554c50216dfac8efcfe987aa8a57d23a02c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+function findMatching(array, name){
+    let matches = array.filter(arr => {
+        return arr.toLowerCase() === name.toLowerCase()
+    })
+    return matches
+}
+
+function fuzzyMatch(array, letter){
+    let newMatch = array.filter(arr => {
+        return arr[0].toLowerCase() === letter[0].toLowerCase()
+    })
+    return newMatch
+}
+
+function matchName(driverArr, name){
+    let newObj = driverArr.filter(arr => {
+        return arr.name === name
+    })
+    return newObj
+}
+
+
+let namesArr = ["Keshav", "Bob", "Shawn", "Kesha", "KEShav", "keshav", "Steve", "Steven"]
+
+let newNamesArr = namesArr.filter(arr => {
+    return arr.toLowerCase() === "Keshav".toLowerCase()
+})
+
+let firstLetters = namesArr.filter(arr => {
+    return arr[0].toLowerCase() === "S".toLowerCase()
+}) 
+
+console.log(newNamesArr)
+console.log(firstLetters)
+console.log(namesArr[0][0])
+
